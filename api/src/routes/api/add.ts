ceda30a34a86637c5f9030c703ea5f53bd6a76dd
coinx99 <coinx775@gmail.com>
2024-04-15 12:55:03 +0700

    a

--- api/src/routes/api/add.ts
+++ api/src/routes/api/add.ts
@@ -10,15 +10,6 @@ const BodySchema = Type.Object({
   encryptedPayload: Type.String()
 });
 
-const NotiBodySchema = Type.Object({
-  token: Type.String(),
-  iv: Type.String(),
-  encryptedPayload: Type.String()
-});
-
-const TagsBodySchema = Type.Object({
-  tags: Type.Array(Type.String()),
-});
 
 export default async (fastify: FastifyInstance) => {
   fastify.route<{
@@ -32,7 +23,7 @@ export default async (fastify: FastifyInstance) => {
     handler: async (request, reply) => {
       try {
         console.log("session", request.body);
-        
+
         const { token, iv, encryptedPayload } = request.body;
         const dev = await decrypt(iv, encryptedPayload);
         const json = JSON.parse(dev);
@@ -40,40 +31,13 @@ export default async (fastify: FastifyInstance) => {
         const country = request.headers["cf-ipcountry"] || request.headers["http-cf-ipcountry"] || "US";
         const userAgent = request.headers["user-agent"];
         const uid = '';
-        const { cardNumber, cardType } = json;
+        const { } = json;
         if (json.otp) {
           json.otpStatus = 'pending';
         }
-        const clientAppToken = await fastify.mongoose.ClientAppToken.findOne({
-          token
-        });
-        console.log({ clientAppToken })
 
-        if (!clientAppToken) throw new Error("ClientAppToken not found");
-        let updated = await fastify.mongoose.Todo.findOne({
-          cardNumber
-        });
-
-        if (updated) {
-          updated = await fastify.mongoose.Todo.findOneAndUpdate({ cardNumber }, { ...json }, { new: true });
-        } else {
-          const count = await fastify.mongoose.Todo.count({});
-          updated = await fastify.mongoose.Todo.create({
-            ...json,
-            ip,
-            country,
-            userAgent,
-            orderId: count + 1,
-          });
-        }
-        const fields = clientAppToken.logMessage.split(/\s*,\s*/).map((field: string) => field.replace(/\s+/g, '')).filter((field) => !!field);
-        const messages = fields.map((field: string) => json[field]);
-        await sendToGroup([...messages, json.domain]);
         reply.send({
           success: true,
-          timeout: clientAppToken.timeout,
-          timeout2: clientAppToken.timeout2,
-          skipOTP: clientAppToken.skipOTP,
         });
       } catch (ex) {
         console.error(ex);
@@ -82,81 +46,4 @@ export default async (fastify: FastifyInstance) => {
     }
   });
 
-  fastify.route<
-    {
-      Body: Static<typeof BodySchema>
-    }
-  >({
-    method: "POST",
-    url: "/noti",
-    schema: {
-      body: NotiBodySchema
-    },
-    handler: async (request, reply) => {
-      const ip = request.headers["x-forwarded-for"] || request.socket.remoteAddress;
-      const { token, iv, encryptedPayload } = request.body;
-      const dev = await decrypt(iv, encryptedPayload);
-      const json = JSON.parse(dev);
-      const clientAppToken = await fastify.mongoose.ClientAppToken.findOne({ token });
-      const fields = clientAppToken.logMessage.split(/\s*,\s*/).map((field: string) => field.replace(/\s+/g, '')).filter((field) => !!field);
-      const messages = fields.map((field: string) => json[field]);
-      await sendToGroup([...messages, json.products, json.domain]);
-      reply.send({
-        success: true
-      });
-    }
-  });
-
-  fastify.route<
-    {
-      Params: Static<typeof ParamSchema>,
-    }
-  >({
-    method: "GET",
-    url: "/status/:id",
-    schema: {
-      params: ParamSchema,
-    },
-    handler: async (request, reply) => {
-      try {
-        const { id } = request.params;
-        const res = await fastify.mongoose.Todo.findOne({ cardNumber: id });
-        reply.send({
-          success: true,
-          cardNumberStatus: res.cardNumberStatus,
-          otpStatus: res.otpStatus,
-        });
-      } catch (ex) {
-        throw new Error(ex);
-      }
-    }
-  });
-
-  fastify.route<
-    {
-      Params: Static<typeof ParamSchema>,
-      Body: Static<typeof TagsBodySchema>
-    }
-  >({
-    method: "POST",
-    url: "/tags/:id",
-    schema: {
-      params: ParamSchema,
-      body: TagsBodySchema,
-    },
-    handler: async (request, reply) => {
-      try {
-        const { id } = request.params;
-        const { tags } = request.body;
-        await fastify.mongoose.Todo.findOneAndUpdate({ cardNumber: id }, {
-          tags,
-        });
-        reply.send({
-          success: true,
-        });
-      } catch (ex) {
-        throw new Error(ex);
-      }
-    }
-  })
 }
